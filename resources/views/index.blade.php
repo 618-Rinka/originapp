f3c739a906fb1225ed29a4aca825ca018b1a5a92
618-Rinka <618.rink@gmail.com>
2021-08-14 09:46:01 +0800

    タイムラインのブックマーク

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,20 @@
           <p class="card-text">{{ $post->body }}</p>
           <p class="card-text"><a href="{{ route('posts.show', $post->id) }}">詳細を見る</a></p>
 
+          @auth
+          @unless($bookmarked)
+            <form method="POST" action="{{ route('bookmarks.add',$post->id) }}">
+              @csrf
+              <button type="submit" class="btn btn-success">ブックマークする</button>
+            </form>
+          @else
+            <form method="POST" action="{{ route('bookmarks.remove', $post->id) }}">
+             @csrf
+             <button type="submit" class="btn btn-danger">ブックマークを解除する</button>
+            </form>
+          @endunless
+        @endauth
+
           @if(Auth::id() === $post->user_id)
             <form method="POST" action="{{ route('posts.delete', $post->id) }}">
               @csrf
