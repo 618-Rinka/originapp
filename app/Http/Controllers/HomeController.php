6572616fcfc56dad82e5b9dab4da3e0a4590f823
618-Rinka <618.rink@gmail.com>
2021-10-09 16:47:02 +0900

    再調整

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,38 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
-<<<<<<< HEAD
-use Illuminate\Http\Request;
-=======
->>>>>>> a1ecf78833055f938e3e54967d9b93037fa4e73f
 use App\Models\Topic;
 
 class HomeController extends Controller
 {
-<<<<<<< HEAD
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    //public function __construct()
-    //{
-        //$this->middleware('auth');
-    //}
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        //return view('home');
-=======
-    public function index()
-    {
->>>>>>> a1ecf78833055f938e3e54967d9b93037fa4e73f
         // NOTE: 最大6件までとする
         $topics = Topic::with(['user'])->orderBy('created_at','desc')->limit(6)->get();
         return view('index', ['topics' => $topics]);
